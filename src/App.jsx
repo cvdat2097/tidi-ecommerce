5ed95f21dc786fef02eda50c0b29536b5e820425
datcv <datcv@vng.com.vn>
2018-11-20 10:17:28 +0700

    add Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 import Header from './layouts/Header/Header';
 import Footer from './layouts/Footer/Footer';
+import Cart from './pages/Cart/Cart';
 
 class App extends Component {
   render() {
     return (
       <div className="main-wrapper">
         <Header />
+        <Cart />
         <div className="main-content">
-        d
+          d
         </div>
         <Footer />
       </div>
---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import './Cart.scss';
+
+const initialState = {
+    isActive: false
+}
+
+export default class Cart extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = initialState;
+
+        setTimeout(() => {
+            this.activeCart();
+        }, 1000)
+    }
+
+    activeCart() {
+        this.setState({
+            isActive: !this.state.isActive
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {/* <!-- ##### Right Side Cart Area ##### --> */}
+                <div className={
+                    "cart-bg-overlay " + (this.state.isActive ? 'cart-bg-overlay-on' : '')
+                }></div>
+
+                <div className={"right-side-cart-area " + (this.state.isActive ? 'cart-on' : '')}>
+                    {/* <!-- Cart Button --> */}
+                    <div className="cart-button">
+                        <a href="/" id="rightSideCart"><img src="img/core-img/bag.svg" alt="" /> <span>3</span></a>
+                    </div>
+
+                    <div className="cart-content d-flex">
+
+                        {/* <!-- Cart List Area --> */}
+                        <div className="cart-list">
+                            {/* <!-- Single Cart Item --> */}
+                            <div className="single-cart-item">
+                                <a href="/" className="product-image">
+                                    <img src="img/product-img/product-1.jpg" className="cart-thumb" alt="" />
+                                    {/* <!-- Cart Item Desc --> */}
+                                    <div className="cart-item-desc">
+                                        <span className="product-remove"><i className="fa fa-close" aria-hidden="true"></i></span>
+                                        <span className="badge">Mango</span>
+                                        <h6>Button Through Strap Mini Dress</h6>
+                                        <p className="size">Size: S</p>
+                                        <p className="color">Color: Red</p>
+                                        <p className="price">$45.00</p>
+                                    </div>
+                                </a>
+                            </div>
+
+                            {/* <!-- Single Cart Item --> */}
+                            <div className="single-cart-item">
+                                <a href="/" className="product-image">
+                                    <img src="img/product-img/product-2.jpg" className="cart-thumb" alt="" />
+                                    {/* <!-- Cart Item Desc --> */}
+                                    <div className="cart-item-desc">
+                                        <span className="product-remove"><i className="fa fa-close" aria-hidden="true"></i></span>
+                                        <span className="badge">Mango</span>
+                                        <h6>Button Through Strap Mini Dress</h6>
+                                        <p className="size">Size: S</p>
+                                        <p className="color">Color: Red</p>
+                                        <p className="price">$45.00</p>
+                                    </div>
+                                </a>
+                            </div>
+
+                            {/* <!-- Single Cart Item --> */}
+                            <div className="single-cart-item">
+                                <a href="/" className="product-image">
+                                    <img src="img/product-img/product-3.jpg" className="cart-thumb" alt="" />
+                                    {/* <!-- Cart Item Desc --> */}
+                                    <div className="cart-item-desc">
+                                        <span className="product-remove"><i className="fa fa-close" aria-hidden="true"></i></span>
+                                        <span className="badge">Mango</span>
+                                        <h6>Button Through Strap Mini Dress</h6>
+                                        <p className="size">Size: S</p>
+                                        <p className="color">Color: Red</p>
+                                        <p className="price">$45.00</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+
+                        {/* <!-- Cart Summary --> */}
+                        <div className="cart-amount-summary">
+
+                            <h2>Summary</h2>
+                            <ul className="summary-table">
+                                <li><span>subtotal:</span> <span>$274.00</span></li>
+                                <li><span>delivery:</span> <span>Free</span></li>
+                                <li><span>discount:</span> <span>-15%</span></li>
+                                <li><span>total:</span> <span>$232.00</span></li>
+                            </ul>
+                            <div className="checkout-btn mt-100">
+                                <a href="checkout.html" className="btn essence-btn">check out</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* <!-- ##### Right Side Cart End ##### --> */}
+            </div>
+        );
+    }
+}
